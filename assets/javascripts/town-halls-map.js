98b6645484c00eea5e4b4038b74b06fef9983300
Annabelle Thomas Taylor <ataylor@mapc.org>
2020-06-24 15:01:03 -0400

    Hide and render selected town hall after guess

--- assets/javascripts/town-halls-map.js
+++ assets/javascripts/town-halls-map.js
@@ -1,6 +1,4 @@
-mapboxgl.accessToken = 'pk.eyJ1IjoiaWhpbGwiLCJhIjoiY2plZzUwMTRzMW45NjJxb2R2Z2thOWF1YiJ9.szIAeMS4c9YTgNsJeG36gg';
-d3.csv('/MapboxEmbeds/assets/data/towncityhalls.csv')
-.then((response) => {
+d3.csv('/MapboxEmbeds/assets/data/towncityhalls.csv').then((response) => {
   let zoom = 9;
   let center = [-71.0408, 42.3317];
   if (window.innerWidth <= 500) {
@@ -10,9 +8,6 @@ d3.csv('/MapboxEmbeds/assets/data/towncityhalls.csv')
     zoom = 8.27;
     center = [-70.89, 42.369];
   }
-  console.log(response)
-  
-
   const townHallMap = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/ihill/ckbtlib0k0zrq1io0rf4viev8',
@@ -24,29 +19,40 @@ d3.csv('/MapboxEmbeds/assets/data/towncityhalls.csv')
       [-74.728, 38.167], // Southwest bound
       [-66.541, 46.032], // Northeast bound
     ],
+    accessToken: 'pk.eyJ1IjoiaWhpbGwiLCJhIjoiY2plZzUwMTRzMW45NjJxb2R2Z2thOWF1YiJ9.szIAeMS4c9YTgNsJeG36gg',
   });
-
+  const draw = new MapboxDraw({
+    displayControlsDefault: false,
+    controls: {}
+  });
+  const scale = new mapboxgl.ScaleControl({
+    maxWidth: 80,
+    unit: 'imperial'
+  });
+  townHallMap.addControl(draw);
+  townHallMap.addControl(scale);
+  scale.setUnit('imperial');
+  const hallList = response.map((row) => {
+    return {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [row.lng, row.lat],
+      },
+      properties: {
+        muni: row.muni,
+        muni_id: +row.muni_id,
+        lng: +row.lng,
+        lat: +row.lat,
+      }
+    }
+  })
 
   townHallMap.on('load', () => {
-    const hallList = response.map((row) => {
-      return {
-        type: 'Feature',
-        geometry: {
-          type: 'Point',
-          coordinates: [row.lng, row.lat],
-        },
-        properties: {
-          muni: row.muni,
-          muni_id: row.muni_id,
-          lng: +row.lng,
-          lat: +row.lat,
-        }
-      }
-    })
-
+    let muniIdToMatch = 35;
     townHallMap.addLayer({
-      'id': 'points',
-      'type': 'circle',
+      'id': 'town-halls',
+      'type': 'symbol',
       'source': {
         'type': 'geojson',
         'data': {
@@ -54,42 +60,37 @@ d3.csv('/MapboxEmbeds/assets/data/towncityhalls.csv')
           'features': hallList
         }
       },
+      'layout': {
+        'icon-image': 'town-hall-15',
+      },
       'paint': {
-        'circle-color': 'red',
-        'circle-radius': 5,
+        'icon-opacity': 0,
       }
     });
 
-    var draw = new MapboxDraw({
-      displayControlsDefault: false,
-      controls: {
-        point: true,
-        trash: true
-      }
-    });
+    draw.changeMode('draw_point');
+    townHallMap.on('draw.create', calculateDistance);
 
-    townHallMap.addControl(draw)
-    var scale = new mapboxgl.ScaleControl({
-      maxWidth: 80,
-      unit: 'imperial'
-      });
-      townHallMap.addControl(scale);
-       
-      scale.setUnit('imperial');
-    townHallMap.on('draw.create', updateArea);
-
-
-    function updateArea(e) {
+    function calculateDistance() {
+      console.log(draw.getSelected())
       const drawnCoords = draw.getAll().features[0].geometry.coordinates;
       const selectedTownHall = townHallMap.queryRenderedFeatures({
-        filter: ["==", "muni_id", "35"]
+        filter: ["==", "muni_id", muniIdToMatch]
       });
+      console.log(selectedTownHall)
       const townHallCoords = [selectedTownHall[0].properties.lng, selectedTownHall[0].properties.lat]
       const linestring = turf.lineString([townHallCoords, drawnCoords]);
       console.log(townHallCoords)
       console.log(drawnCoords)
-      console.log(turf.length(linestring, {units: 'miles'}).toLocaleString())  
+      console.log(turf.length(linestring, {units: 'miles'}).toLocaleString())
 
+      townHallMap.setPaintProperty('town-halls', 'icon-opacity', [
+        "match",
+        ["get", "muni_id"],
+        [muniIdToMatch],
+        1,
+        0
+      ])
     }
   });
 });
