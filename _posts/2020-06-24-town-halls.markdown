7a2fa7c8fe7160a38db364b37aa65c0abdab609d
Annabelle Thomas Taylor <ataylor@mapc.org>
2020-06-24 14:34:05 -0400

    Console log distance between set town hall and drawn point

--- _posts/2020-06-24-town-halls.markdown
+++ _posts/2020-06-24-town-halls.markdown
@@ -5,4 +5,10 @@ description: "Click to view data visualization"
 ---
 <main id="map" class="map"></main>
 
+<link
+rel="stylesheet"
+href="https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-draw/v1.0.9/mapbox-gl-draw.css"
+type="text/css"
+/><script src="https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-draw/v1.0.9/mapbox-gl-draw.js"></script>
 <script src="{{'assets/javascripts/town-halls-map.js' | absolute_url }}" type="module"></script>
+<script src="https://npmcdn.com/@turf/turf@5.1.6/turf.min.js"></script>
--- assets/javascripts/town-halls-map.js
+++ assets/javascripts/town-halls-map.js
@@ -1,5 +1,4 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoiaWhpbGwiLCJhIjoiY2plZzUwMTRzMW45NjJxb2R2Z2thOWF1YiJ9.szIAeMS4c9YTgNsJeG36gg';
-
 d3.csv('/MapboxEmbeds/assets/data/towncityhalls.csv')
 .then((response) => {
   let zoom = 9;
@@ -38,28 +37,59 @@ d3.csv('/MapboxEmbeds/assets/data/towncityhalls.csv')
         },
         properties: {
           muni: row.muni,
-          muni_id: row.muni_id
+          muni_id: row.muni_id,
+          lng: +row.lng,
+          lat: +row.lat,
         }
       }
     })
-  
-  
-    townHallMap.addSource('points', {
-      'type': 'geojson',
-      'data': {
-      'type': 'FeatureCollection',
-      'features': hallList
-      }
-    });
 
     townHallMap.addLayer({
       'id': 'points',
       'type': 'circle',
-      'source': 'points',
+      'source': {
+        'type': 'geojson',
+        'data': {
+          'type': 'FeatureCollection',
+          'features': hallList
+        }
+      },
       'paint': {
         'circle-color': 'red',
         'circle-radius': 5,
       }
     });
-  })
+
+    var draw = new MapboxDraw({
+      displayControlsDefault: false,
+      controls: {
+        point: true,
+        trash: true
+      }
+    });
+
+    townHallMap.addControl(draw)
+    var scale = new mapboxgl.ScaleControl({
+      maxWidth: 80,
+      unit: 'imperial'
+      });
+      townHallMap.addControl(scale);
+       
+      scale.setUnit('imperial');
+    townHallMap.on('draw.create', updateArea);
+
+
+    function updateArea(e) {
+      const drawnCoords = draw.getAll().features[0].geometry.coordinates;
+      const selectedTownHall = townHallMap.queryRenderedFeatures({
+        filter: ["==", "muni_id", "35"]
+      });
+      const townHallCoords = [selectedTownHall[0].properties.lng, selectedTownHall[0].properties.lat]
+      const linestring = turf.lineString([townHallCoords, drawnCoords]);
+      console.log(townHallCoords)
+      console.log(drawnCoords)
+      console.log(turf.length(linestring, {units: 'miles'}).toLocaleString())  
+
+    }
+  });
 });
