8d0f3df4a19d037e9e04ea203d08e477cbf62fb0
Annabelle Thomas Taylor <ataylor@mapc.org>
2020-06-25 16:19:43 -0400

    Set bounding box to geocoder

--- assets/javascripts/zoning-map.js
+++ assets/javascripts/zoning-map.js
@@ -11,7 +11,7 @@ if (window.innerWidth <= 500) {
 
 const map = new mapboxgl.Map({
   container: 'map',
-  style: 'mapbox://styles/ihill/ckb7xc2iq1sxf1ip9rdi5x0u6/',
+  style: 'mapbox://styles/ihill/ck92yirkh2mt71ho83t7y60m9',
   center,
   zoom,
   minZoom: 6,
@@ -25,8 +25,30 @@ const map = new mapboxgl.Map({
 
 
 map.addControl(new mapboxgl.NavigationControl(), 'bottom-right');
+map.addControl(new mapboxgl.GeolocateControl(), 'top-left');
+map.addControl(
+  new MapboxGeocoder({
+    accessToken: mapboxgl.accessToken,
+    mapboxgl: mapboxgl,
+    bbox: [-73.613892, 41.199323, -69.631348, 42.960443], // Northeast bound
+  }), 'top-left'
+);
 
 map.on('load', () => {
+  map.addLayer({
+    id: 'Testing Centers',
+    type: 'circle',
+    source: {
+      type: 'geojson',
+      data: 'https://services1.arcgis.com/TXaY625xGc0yvAuQ/arcgis/rest/services/Test_Sites_Public/FeatureServer/0/query?where=OBJECTID_1+%3D+OBJECTID_1&outfields=*&f=pgeojson',
+    },
+    paint: {
+      'circle-color': '#FFFFFF',
+      'circle-radius': 4,
+      'circle-stroke-width': 1,
+      'circle-stroke-color': '#03332D',
+    },
+  });
   document.querySelector('.print').addEventListener('click', (e) => {
     var img = map.getCanvas().toDataURL('image/png')
     document.querySelector('.print').href = img
