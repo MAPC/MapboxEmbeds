62866831cfaf08845591877ad26f8c7fc5b7a647
Annabelle Thomas Taylor <ataylor@mapc.org>
2020-07-24 14:27:46 -0400

    Update legend and tooltip

--- assets/javascripts/ppp-map.js
+++ assets/javascripts/ppp-map.js
@@ -2,8 +2,9 @@ const legend1 = document.querySelector('#legend1');
 const legend2 = document.querySelector('#legend2');
 const legend3 = document.querySelector('#legend3');
 const legend4 = document.querySelector('#legend4');
+const currentNaicsCode = document.querySelector('.legend__select')
 
-d3.csv('/MapboxEmbeds/assets/data/PPP-data-up-to-and-over-150K-MA-city-clean-joined.csv')
+d3.csv('https://raw.githubusercontent.com/MAPC/paycheck-protection-program-ma/master/PPP-data-joined.csv')
 .then((response) => {
   let map = new mapboxgl.Map({
     container: 'map',
@@ -27,6 +28,26 @@ d3.csv('/MapboxEmbeds/assets/data/PPP-data-up-to-and-over-150K-MA-city-clean-joi
         muni: loan.City,
         loans: {
           total: 0,
+          11: 0,
+          21: 0,
+          22: 0,
+          23: 0,
+          31: 0, // 31, 32, 33
+          42: 0,
+          44: 0, // 44-45
+          48: 0, //48-49
+          51: 0,
+          52: 0,
+          53: 0,
+          54: 0,
+          55: 0,
+          56: 0,
+          61: 0,
+          62: 0,
+          71: 0,
+          72: 0,
+          81: 0,
+          92: 0,
         },
         establishments: {
           total: 0,
@@ -34,19 +55,19 @@ d3.csv('/MapboxEmbeds/assets/data/PPP-data-up-to-and-over-150K-MA-city-clean-joi
       }
       municipalities.push(municipality)
     }
+    municipality.loans.total += 1;
     if (!municipality.loans[`${loan.NAICSCode2}`]) {
       municipality.loans[`${loan.NAICSCode2}`] = 1;
     } else {
       municipality.loans[`${loan.NAICSCode2}`] += 1;
     }
+    
     if (!municipality.establishments[`${loan.NAICSCode2}`] && loan.estab !== 'NA') {
       municipality.establishments.total += +loan.estab
       municipality.establishments[`${loan.NAICSCode2}`] = +loan.estab;
     }
-    municipality.loans.total += 1;
     return municipalities
   }, []);
-
 
   console.log(loansByNaicsAndMuni)
 
@@ -81,6 +102,25 @@ d3.csv('/MapboxEmbeds/assets/data/PPP-data-up-to-and-over-150K-MA-city-clean-joi
         'fill-outline-color': 'black',
       }
     })
+    map.moveLayer('MAPC outline')
+
+    map.on('click', 'Muni choropleth', function(e) {
+      const pppPercentage = loansByNaicsAndMuni.find(row => row.muni === e.features[0].properties.muni)
+      const borrowers = pppPercentage.loans[`${currentNaicsCode.value}`]
+      const establishments = pppPercentage.establishments[`${currentNaicsCode.value}`]
+      console.log(pppPercentage)
+      const tooltipHtml = `
+        <p class="tooltip__title tooltip__title--datacommon">${e.features[0].properties.muni}</p>
+        <p class="tooltip__title tooltip__title--datacommon">${+borrowers} loans</p>
+        <p class="tooltip__title tooltip__title--datacommon">${+establishments} total establishments (2018 ES-202)</p>
+        <p class="tooltip__title tooltip__title--datacommon">${d3.format('.1%')(+borrowers / +establishments)} of establishments covered</p>
+    `;
+      new mapboxgl.Popup()
+        .setLngLat(e.lngLat)
+        .setHTML(tooltipHtml)
+        .addTo(map);
+    })
+
   });
 })
 
