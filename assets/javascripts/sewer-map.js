872914e777eef9c1503da8241f7511b2b5b6844f
Annabelle Thomas Taylor <ataylor@mapc.org>
2021-03-24 10:19:26 -0400

    Add data edits and MWRA distinction to April 2021 map

--- assets/javascripts/sewer-map.js
+++ assets/javascripts/sewer-map.js
@@ -1,4 +1,4 @@
-const colorScale = ['#3B66B0', '#FDB525', '#4FB965'];
+const colorScale = ['#FDB525', '#0097C4', '#92C9ED', '#4FB965']
 const dataNa = '#B6B6B6';
 let zoom = 9;
 let center = [-71.0408, 42.3317];
@@ -14,7 +14,7 @@ d3.csv('/MapboxEmbeds/assets/data/WasteWater_System.csv')
 .then((response) => {
   const map = new mapboxgl.Map({
     container: 'map',
-    style: "mapbox://styles/ihill/ckmeybo3w0hse17pcxgsrd9g4",
+    style: "mapbox://styles/ihill/ckmeybo3w0hse17pcxgsrd9g4?fresh=true",
     accessToken: "pk.eyJ1IjoiaWhpbGwiLCJhIjoiY2plZzUwMTRzMW45NjJxb2R2Z2thOWF1YiJ9.szIAeMS4c9YTgNsJeG36gg",
     center,
     zoom,
@@ -27,32 +27,50 @@ d3.csv('/MapboxEmbeds/assets/data/WasteWater_System.csv')
   });
 
   map.addControl(new mapboxgl.NavigationControl(), 'bottom-right');
-  const choropleth = (value) => {
+  const choropleth = (value, ww_provider) => {
     if (value === 0) { // no centralized sewer lines, blue
       return colorScale[0]
-    } if (value === 1) { // fully sewered, yellow
+    } if (value === 1 && ww_provider !== 'MWRA') { // fully sewered, MWRA, yellow
       return colorScale[1]
-    } // Partially sewered, green
-    return colorScale[2]
+    } if (value === 1 && ww_provider === 'MWRA') { // fully sewered, not MWRA, red
+      return colorScale[2]
+    }
+    // Partially sewered, green
+    return colorScale[3]
   }
 
-  const sewerFlagFunc = (value) => {
+  const iconChoropleth = (value) => {
+    if (value === 0) {
+      return 'dot-11-white'
+    }
+    return 'blank'
+  }
+
+  const sewerFlagFunc = (value, ww_provider) => {
     if (value === 0) {
       return 'No centralized sewer system(s)'
-    } if (value === 1) {
-      return 'Fully sewered, or nearly so'
+    } if (value === 1 && ww_provider === 'MWRA') {
+      return 'Fully sewered, or nearly so (provided by MWRA)'
+    } if (value === 1 && ww_provider !== 'MWRA') {
+      return 'Fully sewered, or nearly so (not provided by MWRA)'
     }
     return 'Partially sewered'
   }
+
   const sewerChoropleth = ['match', ['get', 'municipal']];
+  const dataChoropleth = ['match', ['get', 'municipal']];
   const muniInfo = {};
   response.forEach((row) => {
-    sewerChoropleth.push(row.municipal, row.sewer_flag ? choropleth(+row.sewer_flag) : dataNa);
-    muniInfo[row.municipal] = sewerFlagFunc(+row.sewer_flag);
+    sewerChoropleth.push(row.municipal, row.sewer_flag ? choropleth(+row.sewer_flag, row['WW provider']) : dataNa);
+    dataChoropleth.push(row.municipal, row.Data_flag ? iconChoropleth(+row.Data_flag) : 'blank')
+    muniInfo[row.municipal] = sewerFlagFunc(+row.sewer_flag, row['WW provider']);
   });
   sewerChoropleth.push(dataNa);
+  dataChoropleth.push('blank')
+  console.log(dataChoropleth)
 
     map.on('load', () => {
+      console.log(map.getStyle())
       map.resize();
       map.addSource('MAPC Municipalities', {
         type: 'vector',
@@ -77,18 +95,37 @@ d3.csv('/MapboxEmbeds/assets/data/WasteWater_System.csv')
         source: 'Sewer Lines',
         'source-layer': "Sewer Lines",
         type: 'line',
-        paint: { 'line-color': 'black', 'line-width': .25 }
+        paint: { 'line-color': '#231F20', 'line-width': .25 }
+      })
+
+      map.addLayer({
+        id: 'MAPC Muni Data layer',
+        type: 'fill',
+        source: 'MAPC Municipalities',
+        'source-layer': 'MAPC_borders-0im3ea',
+        paint: { 'fill-pattern': dataChoropleth }
       })
 
       map.on('click', (e) => {
         const clickedData = map.queryRenderedFeatures([e.point.x, e.point.y], { layers: ['MAPC Municipalities layer'] });
         if (clickedData.length > 0) {
+          let tooltipHtml = `
+            <p class="tooltip__title tooltip__title--datacommon">${clickedData[0].properties.municipal}</p>
+            <p class="tooltip__text tooltip__text--datacommon">${muniInfo[clickedData[0].properties.municipal]}<p>
+          `
+          if (clickedData[0].properties.municipal === 'Hingham') {
+            tooltipHtml += `
+              <br />
+              <p class="tooltip__text tooltip__text--datacommon">
+                <em>
+                  Note: Hingham has two separate sewer districts, the larger of which is served by the MWRA. <a href="https://www.hingham-ma.gov/226/Sewer-Commission" target="_parent">Learn more</a>
+                </em>
+              </p>
+            `
+          }
           new mapboxgl.Popup()
             .setLngLat(e.lngLat)
-            .setHTML(`
-              <p class="tooltip__title tooltip__title--datacommon">${clickedData[0].properties.municipal}</p>
-              <p class="tooltip__text tooltip__text--datacommon">${muniInfo[clickedData[0].properties.municipal]}<p>
-            `)
+            .setHTML(tooltipHtml)
             .addTo(map);
         }
       });
